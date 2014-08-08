7c2970d03f128db2c3db275ab9c3a281ffd67e8a
Mahmood Khan <mkhan@mercycorps.org>
2014-08-07 22:48:56 -0700

    used class based view in url.conf and created about.html page

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -1,11 +1,13 @@
+from django.views.generic import TemplateView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'blog.views.index', name='home'),
-    url(r'^admin/', include(admin.site.urls)),
+	url(r'^admin/', include(admin.site.urls)),
+	url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
-    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    #url(r'^$', 'blog.views.index', name='home'),
+    url(r'^$', TemplateView.as_view(template_name="blog/index.html")),
 )
