ddad1ae04dc6f4722587fec692737fc14a1170fd
Mahmood Khan <mkhan@mercycorps.org>
2014-08-07 22:51:26 -0700

    replaced tabs with spaces

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -5,8 +5,8 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-	url(r'^admin/', include(admin.site.urls)),
-	url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^(?P<slug>[\w\-]+)/$', 'blog.views.post'),
     #url(r'^$', 'blog.views.index', name='home'),
     url(r'^$', TemplateView.as_view(template_name="blog/index.html")),
