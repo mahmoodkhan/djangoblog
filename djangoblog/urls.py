b013434df09e9c6cffd9cd8f7cbcaa7686daa197
Mahmood Khan <mkhan@mercycorps.org>
2014-09-05 17:08:41 -0700

    added generic date based views and enhanced html template

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -1,5 +1,5 @@
 from django.views.generic import TemplateView
-from django.views.generic.dates import ArchiveIndexView
+from django.views.generic.dates import ArchiveIndexView, MonthArchiveView
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from blog.views import *
@@ -14,7 +14,8 @@ urlpatterns = patterns('',
     url(r'^about/$', TemplateView.as_view(template_name='about.html'), name='about'),
     url(r'^contact/$', ContactView.as_view(), name='contact'),
     url(r'^newpost/$', BlogPostCreateView.as_view(), name='newblogpost'),
-    url(r'^archive/$', ArchiveIndexView.as_view(model=BlogPost, date_field="pub_date"), name='blogpost_archive'),
+    url(r'^archive/index/$', ArchiveIndexView.as_view(model=BlogPost, date_field="pub_date"), name='blogpost_archive'),
+    url(r'^archive/monthly/$', MonthArchiveView.as_view(model=BlogPost, date_field="pub_date", paginate_by=12), name='monthly'),
     
     # To view a blog-post by slug in the url
     #url(r'^detailpost/(?P<slug>[\w\-]+)/$', BlogPostDetail.as_view(), name='detailpost'),
