c78a0dbb981ea2f9b0cf0cba9351e6d2ae6fe044
Mahmood Khan <mkhan@mercycorps.org>
2014-09-18 17:40:44 -0700

    got ready for apache deployment

--- djangoblog/wsgi.py
+++ djangoblog/wsgi.py
@@ -7,7 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+ROOT_PATH = os.path.join(PROJECT_PATH, '../')
+sys.path.append(ROOT_PATH)
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangoblog.settings")
 
 from django.core.wsgi import get_wsgi_application
