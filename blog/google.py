c6bf7ea6ebb825c7af1ec524ccb23a83bd33c47d
Mahmood Khan <mkhan1484@gmail.com>
2014-12-21 23:15:23 -0800

    made a bit more progress on google+ sign-in

--- blog/google.py
+++ blog/google.py
@@ -40,32 +40,46 @@ from django.views.decorators.csrf import csrf_protect
 class GoogleSingInView(TemplateView):
     template_name="plus.html"
     
-    def get(self, request, *args, **kwargs):
+    #def get(self, request, *args, **kwargs):
         # Create a state token to prevent request forgery.
         # Store it in the session for later validation.
-        state = ''.join(random.choice(string.ascii_uppercase + string.digits)
-                  for x in range(32))
-        request.session['be'] = state
-        request.session['me'] = "oauth2callback"
+        #state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+        #          for x in range(32))
+        #request.session['be'] = state
+        #request.session['me'] = "oauth2callback"
         #return render(request, self.template_name, {})
-        print("GET STATE")
-        print(request.session['be'])
-        print("GET TEST")
-        print(request.session['me'])
-        return super(GoogleSingInView, self).get(request, *args, **kwargs)
+        #print("GET STATE")
+        #print(request.session['be'])
+        #print("GET TEST")
+        #print(request.session['me'])
+        #return super(GoogleSingInView, self).get(request, *args, **kwargs)
     @method_decorator(csrf_protect)
     def post(self, request, *args, **kwargs):
         code = request.POST.get("code", None)
-        print("POST STATE: ")
-        print(request.session['be'])
-        print("TEST")
-        print(request.session['me'])
+        #print("POST STATE: ")
+        #print(request.session['be'])
+        #print("TEST")
+        #print(request.session['me'])
+        if code:
+            try:
+                oauth_flow = flow_from_clientsecrets('blog/client_secrets.json', scope='')
+                oauth_flow.redirect_uri = 'postmessage'
+                credentials = oauth_flow.step2_exchange(code)
+                #return HttpResponse(credentials)
+            except FlowExchangeError as e:
+                return HttpResponse("Failed to upgrade the authorization code. 401 %s" % e)
+                #return render(request, self.template_name, {})
+        else:
+            return HttpResponse ("No Code")
 
-        try:
-            oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
-            oauth_flow.redirect_uri = 'postmessage'
-            credentials = oauth_flow.step2_exchange(code)
-            return credentials
-        except:
-            return HttpResponse("Failed to upgrade the authorization code. 401")
-            #return render(request, self.template_name, {})
+         # An ID Token is a cryptographically-signed JSON object encoded in base 64.
+        # Normally, it is critical that you validate an ID Token before you use it,
+        # but since you are communicating directly with Google over an
+        # intermediary-free HTTPS channel and using your Client Secret to
+        # authenticate yourself to Google, you can be confident that the token you
+        # receive really comes from Google and is valid. If your server passes the
+        # ID Token to other components of your app, it is extremely important that
+        # the other components validate the token before using it.
+        gplus_id = credentials.id_token['sub']
+        print(gplus_id)
+        return HttpResponse(credentials)
